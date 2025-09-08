8e26eb8f6884010562c622235875c931de2a0cd5
Shu-Jung Han <sh2329@cornell.edu>
2025-09-07 22:05:19 -0400

    clean unuse

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -15,7 +15,4 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv
     && echo 'complete -C '/usr/local/bin/aws_completer' aws' >> ~/.bashrc \
     && rm -rf awscliv2.zip ./aws
 
-# echo 'export OPENAI_API_KEY="YOUR_OPENAI_API_KEY"' >> ~/.bashrc
-# echo 'export OPENAI_BASE_URL="https://api.ai.it.cornell.edu/"' >> ~/.bashrc
-# echo 'export TZ="America/New_York"' >> ~/.bashrc
-# source ~/.bashrc
+
