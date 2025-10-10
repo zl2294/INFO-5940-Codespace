f78ed3578a72768f82ed5fe40fd404c8d71e9d73
Shu-Jung Han <sh2329@cornell.edu>
2025-10-09 20:26:31 -0400

    clean env setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,7 +8,6 @@ sudo apt-get install -y \
     gcc \
 
 pip install -r requirements.txt
-
 ARCHFLAGS="-arch x86_64" pip install "chromadb>=0.5" langchain_chroma langgraph
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip" \
