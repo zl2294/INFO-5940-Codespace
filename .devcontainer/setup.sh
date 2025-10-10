d9365103d0945ec9989fec1ab36120945c3a6435
Shu-Jung Han <sh2329@cornell.edu>
2025-10-09 20:35:36 -0400

    remove AWS

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,10 +10,10 @@ sudo apt-get install -y \
 pip install -r requirements.txt
 ARCHFLAGS="-arch x86_64" pip install "chromadb>=0.5" langchain_chroma langgraph
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip" \
-    && unzip -o awscliv2.zip \
-    && sudo ./aws/install --update \
-    && echo 'complete -C '/usr/local/bin/aws_completer' aws' >> ~/.bashrc \
-    && rm -rf awscliv2.zip ./aws
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip" \
+#     && unzip -o awscliv2.zip \
+#     && sudo ./aws/install --update \
+#     && echo 'complete -C '/usr/local/bin/aws_completer' aws' >> ~/.bashrc \
+#     && rm -rf awscliv2.zip ./aws
 
 
